f4632dc8e2ce6c14a2afae011b19811322ade034
bludot <admin@bludotos.com>
2013-09-13 22:47:08 -0400

    Update scrolly.js

--- scrolly.js
+++ scrolly.js
@@ -1,50 +1,50 @@
 function scrolly(node, e)
 {
-	//function to scroll on mouse wheel
+	// function to scroll on mouse wheel
 	var scroll = function(node, black, e)
 	{
 		e = e || window.event;
-		//check blacklist for nodes you dont want scrolled
+		// check blacklist for nodes you dont want scrolled
 		if(black.indexOf(e.target.className) === -1 || black.indexOf(e.target.tagName) === -1 || node.running)
 		{
-			//set node.running for scrollbar display
+			// set node.running for scrollbar display
 			node.running = true;
-			clearInterval(window.timeout);
-			node.parentNode.querySelector('.scrollbar').style.opacity = 1;
+			clearInterval(node.timeout);
+			scrollB.style.opacity = 1;
 			window.tempit = node;
-			//mousewheel
+			// mousewheel
 			node.scrollTop += -(e.detail * -10) ? -(e.detail * -10) : -(e.wheelDelta);
-			//set height of scrollbar if content height changes
-			//get percent of content height relative to scrollHeight. multiply by content
-			//height to set the correct scrollbar height
-			node.parentNode.querySelector('.scrollbar').style.height = (node.clientHeight / node.scrollHeight) * node.clientHeight + 'px';
-			//set offsetTop of the scrollbar when you scroll
-			//percent of scrollTop relative to the height of the content.
-			//then multiplied by the scrollbar height
-			node.parentNode.querySelector('.scrollbar').style.top = (node.scrollTop / node.clientHeight) * node.parentNode.querySelector('.scrollbar').clientHeight + 'px';
-			//timeout for scrollbar display
-			//when scrolling stops and doesnt continue: hide the scrollbar
-			window.timeout = setTimeout(function(e)
+			// set height of scrollbar if content height changes
+			// get percent of content height relative to scrollHeight. multiply by content
+			// height to set the correct scrollbar height
+			scrollB.style.height = (node.clientHeight / node.scrollHeight) * node.clientHeight + 'px';
+			// set offsetTop of the scrollbar when you scroll
+			// percent of scrollTop relative to the height of the content.
+			// then multiplied by the scrollbar height
+			scrollB.style.top = (node.scrollTop / node.clientHeight) * scrollB.clientHeight + 'px';
+			// timeout for scrollbar display
+			// when scrolling stops and doesnt continue: hide the scrollbar
+			node.timeout = setTimeout(function(e)
 			{
 				delete node.running;
-				node.parentNode.querySelector('.scrollbar').style.opacity = 0;
+				scrollB.style.opacity = 0;
 			},500);
 		}
-		//prevent body scrolling
+		// prevent body scrolling
 		e.preventDefault();
 		return false;
 	};
 	
 	
-	//create container to hold the scrollbar and content
+	// create container to hold the scrollbar and content
 	var div = document.createElement('div');
 	div.className = 'content';
 	
-	//get classname of the node to get the style
+	// get classname of the node to get the style
 	var className  = node.className;
 	var tmpstyle = '';
 	
-	//function to get the style of the content
+	// function to get the style of the content
 	function getStyle(className) {
 		var classes = document.styleSheets[0].rules || document.styleSheets[0].cssRules;
 		for(var x=0;x<classes.length;x++) {
@@ -54,29 +54,29 @@ function scrolly(node, e)
 		}
 	}
 	
-	//set style of content to container
+	// set style of content to container
 	tmpstyle = getStyle('.test');
 	tmpstyle = tmpstyle.split('{ ')[1].split(' }')[0];
 	div.style.cssText = tmpstyle;
 	div.style.overflow = 'hidden';
 	
-	//add scrollcontainer
+	// add scrollcontainer
 	var scrollC = document.createElement('div');
 	scrollC.className = 'scrollbar-container';
 	scrollC.style.cssText = 'right:0px;';
 	
-	//add scrollbar
+	// add scrollbar
 	var scrollB = document.createElement('div');
 	scrollB.className = 'scrollbar';
 	scrollB.style.cssText = 'height:10px;top:0px;';
 	
-	//copy content node to add to the container
+	// copy content node to add to the container
 	var tmpdiv = node.cloneNode(true);
 	
-	//restore style
+	// restore style
 	tmpdiv.style.cssText = tmpstyle;
 	
-	//create container with scrollbar and content
+	// create container with scrollbar and content
 	node.parentNode.appendChild(div);
 	node.parentNode.removeChild(node);
 	div.appendChild(scrollC);
@@ -85,13 +85,13 @@ function scrolly(node, e)
 	node = tmpdiv;
 	node.style.width = 'auto';
 	
-	//start assigning events to the container and scrollbar
+	// start assigning events to the container and scrollbar
 	e = e || window.event;
 	
-	// set scrollbar height
-	node.parentNode.querySelector('.scrollbar').style.height = (node.clientHeight / node.scrollHeight) * node.clientHeight + 'px';
+	//  set scrollbar height
+	scrollB.style.height = (node.clientHeight / node.scrollHeight) * node.clientHeight + 'px';
 	
-	//on mouse wheeel
+	// on mouse wheeel
 	node.addEventListener('mousewheel',  function(e) {
 		scroll(node, ['body'], e);
 	}, false);
@@ -99,50 +99,50 @@ function scrolly(node, e)
 		scroll(node, ['body'], e);
 	}, false);
 	
-	//on mouse move for showing scrollbar near the side
+	// on mouse move for showing scrollbar near the side
 	node.onmousemove = function(e) {
 		if(e.clientX>(node.clientWidth-10))
 		{
-			node.parentNode.querySelector('.scrollbar').style.opacity = 1;
+			scrollB.style.opacity = 1;
 		} else {
 			if(node.running === false)
 			{
-				node.parentNode.querySelector('.scrollbar').style.opacity = 0;
+				scrollB.style.opacity = 0;
 			}
 		}
 	};
 	
-	// on mouse down for clicking on the scrollbar
-	node.parentNode.querySelector('.scrollbar').onmousedown = function(e) {
+	//  on mouse down for clicking on the scrollbar
+	scrollB.onmousedown = function(e) {
 		
-		//prevent text selection
+		// prevent text selection
 		e.preventDefault();
 		
-		//scrollbar is being used
+		// scrollbar is being used
 		node.running = true;
 		
-		//save coordinates for later use (find change of mouse movement)
+		// save coordinates for later use (find change of mouse movement)
 		var tempY = e.pageY;
-		var tempTop = node.parentNode.querySelector('.scrollbar').offsetTop;
+		var tempTop = scrollB.offsetTop;
 
-		//move scrollbar when mouse down and mouse move
+		// move scrollbar when mouse down and mouse move
 		window.onmousemove = function(e) {
 			
-			//temp variable to use when checking if the scrollbar is too high or too low
+			// temp variable to use when checking if the scrollbar is too high or too low
 			var styleTop = tempTop-(tempY-e.pageY);
 			
-			//set the scrollbar offsetTop
-			node.parentNode.querySelector('.scrollbar').style.top = (styleTop > 0 && styleTop < (node.clientHeight - node.parentNode.querySelector('.scrollbar').clientHeight)) ? styleTop + 'px' : node.parentNode.querySelector('.scrollbar').offsetTop;
+			// set the scrollbar offsetTop
+			scrollB.style.top = (styleTop > 0 && styleTop < (node.clientHeight - scrollB.clientHeight)) ? styleTop + 'px' : scrollB.offsetTop;
 			
-			//set the scrollTop of content if it is greater than '0'
-			var setTop = (node.parentNode.querySelector('.scrollbar').offsetTop / node.parentNode.querySelector('.scrollbar').clientHeight) * node.clientHeight;
+			// set the scrollTop of content if it is greater than '0'
+			var setTop = (scrollB.offsetTop / scrollB.clientHeight) * node.clientHeight;
 			node.scrollTop = setTop > 0 ? setTop : 0;
 			
-			//prevent text selection
+			// prevent text selection
 			e.preventDefault();
 		};
 		
-		//cancel mouse movement
+		// cancel mouse movement
 		window.onmouseup = function(e) {
 			node.running = false;
 			document.onselectstart = function(e) {
@@ -152,14 +152,14 @@ function scrolly(node, e)
 		};
 	};
 	
-	//mouse out hide scrollbar
+	// mouse out hide scrollbar
 	node.onmouseout = function(e) {
 		if(!node.running)
 		{
-			node.parentNode.querySelector('.scrollbar').style.opacity = 0;
+			scrollB.style.opacity = 0;
 		}
-		node.parentNode.querySelector('.scrollbar').onmouseover = function() {
-			node.parentNode.querySelector('.scrollbar').style.opacity = 1;
+		scrollB.onmouseover = function() {
+			scrollB.style.opacity = 1;
 		};
 	};
 }
