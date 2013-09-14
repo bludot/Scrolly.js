086cc997baeab852632b424eb246cb89a39971b7
bludot <admin@bludotos.com>
2013-09-13 22:32:58 -0400

    Add comments
    Comments to help understand what happens

--- scrolly.js
+++ scrolly.js
@@ -13,12 +13,17 @@ function scrolly(node, e)
 			node.parentNode.querySelector('.scrollbar').style.opacity = 1;
 			window.tempit = node;
 			//mousewheel
-			node.scrollTop += -(e.detail * -10) ? -(e.detail * -10) : -(e.wheelDelta / 5);
+			node.scrollTop += -(e.detail * -10) ? -(e.detail * -10) : -(e.wheelDelta);
 			//set height of scrollbar if content height changes
+			//get percent of content height relative to scrollHeight. multiply by content
+			//height to set the correct scrollbar height
 			node.parentNode.querySelector('.scrollbar').style.height = (node.clientHeight / node.scrollHeight) * node.clientHeight + 'px';
-			//set scrolltop
+			//set offsetTop of the scrollbar when you scroll
+			//percent of scrollTop relative to the height of the content.
+			//then multiplied by the scrollbar height
 			node.parentNode.querySelector('.scrollbar').style.top = (node.scrollTop / node.clientHeight) * node.parentNode.querySelector('.scrollbar').clientHeight + 'px';
 			//timeout for scrollbar display
+			//when scrolling stops and doesnt continue: hide the scrollbar
 			window.timeout = setTimeout(function(e)
 			{
 				delete node.running;
@@ -29,12 +34,17 @@ function scrolly(node, e)
 		e.preventDefault();
 		return false;
 	};
+	
+	
 	//create container to hold the scrollbar and content
 	var div = document.createElement('div');
 	div.className = 'content';
+	
+	//get classname of the node to get the style
 	var className  = node.className;
 	var tmpstyle = '';
-	//get style of the content
+	
+	//function to get the style of the content
 	function getStyle(className) {
 		var classes = document.styleSheets[0].rules || document.styleSheets[0].cssRules;
 		for(var x=0;x<classes.length;x++) {
@@ -43,21 +53,30 @@ function scrolly(node, e)
 			}
 		}
 	}
+	
 	//set style of content to container
 	tmpstyle = getStyle('.test');
 	tmpstyle = tmpstyle.split('{ ')[1].split(' }')[0];
 	div.style.cssText = tmpstyle;
 	div.style.overflow = 'hidden';
+	
 	//add scrollcontainer
 	var scrollC = document.createElement('div');
 	scrollC.className = 'scrollbar-container';
 	scrollC.style.cssText = 'right:0px;';
+	
 	//add scrollbar
 	var scrollB = document.createElement('div');
 	scrollB.className = 'scrollbar';
 	scrollB.style.cssText = 'height:10px;top:0px;';
+	
+	//copy content node to add to the container
 	var tmpdiv = node.cloneNode(true);
+	
+	//restore style
 	tmpdiv.style.cssText = tmpstyle;
+	
+	//create container with scrollbar and content
 	node.parentNode.appendChild(div);
 	node.parentNode.removeChild(node);
 	div.appendChild(scrollC);
@@ -65,9 +84,13 @@ function scrolly(node, e)
 	div.appendChild(tmpdiv);
 	node = tmpdiv;
 	node.style.width = 'auto';
+	
+	//start assigning events to the container and scrollbar
 	e = e || window.event;
+	
 	// set scrollbar height
 	node.parentNode.querySelector('.scrollbar').style.height = (node.clientHeight / node.scrollHeight) * node.clientHeight + 'px';
+	
 	//on mouse wheeel
 	node.addEventListener('mousewheel',  function(e) {
 		scroll(node, ['body'], e);
@@ -75,6 +98,7 @@ function scrolly(node, e)
 	node.addEventListener('DOMMouseScroll',  function(e) {
 		scroll(node, ['body'], e);
 	}, false);
+	
 	//on mouse move for showing scrollbar near the side
 	node.onmousemove = function(e) {
 		if(e.clientX>(node.clientWidth-10))
@@ -87,21 +111,38 @@ function scrolly(node, e)
 			}
 		}
 	};
+	
 	// on mouse down for clicking on the scrollbar
 	node.parentNode.querySelector('.scrollbar').onmousedown = function(e) {
+		
+		//prevent text selection
+		e.preventDefault();
+		
+		//scrollbar is being used
 		node.running = true;
-		document.onselectstart = function(e) {
-			return false;
-		};
+		
+		//save coordinates for later use (find change of mouse movement)
 		var tempY = e.pageY;
 		var tempTop = node.parentNode.querySelector('.scrollbar').offsetTop;
+
 		//move scrollbar when mouse down and mouse move
 		window.onmousemove = function(e) {
+			
+			//temp variable to use when checking if the scrollbar is too high or too low
 			var styleTop = tempTop-(tempY-e.pageY);
+			
+			//set the scrollbar offsetTop
 			node.parentNode.querySelector('.scrollbar').style.top = (styleTop > 0 && styleTop < (node.clientHeight - node.parentNode.querySelector('.scrollbar').clientHeight)) ? styleTop + 'px' : node.parentNode.querySelector('.scrollbar').offsetTop;
+			
+			//set the scrollTop of content if it is greater than '0'
 			var setTop = (node.parentNode.querySelector('.scrollbar').offsetTop / node.parentNode.querySelector('.scrollbar').clientHeight) * node.clientHeight;
 			node.scrollTop = setTop > 0 ? setTop : 0;
+			
+			//prevent text selection
+			e.preventDefault();
 		};
+		
+		//cancel mouse movement
 		window.onmouseup = function(e) {
 			node.running = false;
 			document.onselectstart = function(e) {
@@ -110,6 +151,7 @@ function scrolly(node, e)
 			window.onmousemove = null;
 		};
 	};
+	
 	//mouse out hide scrollbar
 	node.onmouseout = function(e) {
 		if(!node.running)
